99cba6e1dc9b19064ac9fb3ccce7dcaaaa79bb95
¨Vuk <GlisovicVuk@gmail.com>
2017-12-16 12:58:19 +0100

    restructured a bit and created test folder

--- solver/create.py
+++ solver/create.py
@@ -4,4 +4,12 @@ Module that makes it easy to create a sudoku field that can be used to solve the
 
 import numpy as np
 
+class SuDoKu:
 
+    def __init__(self, field):
+        self.field = field
+        self.needed_numbers = set(range(1, 10))
+
+    def is_solved(self):
+        # check rows
+        self.field
---
+++ solver/tests/test_sudoku.py
@@ -0,0 +1,23 @@
+import unittest
+import numpy as np
+
+from solver.create import SuDoKu
+
+
+class TestSuDoKu(unittest.TestCase):
+
+    def setUp(self):
+        solved_field = np.array([[1,2,3,4,5,6,7,8,9],
+                                 [4,5,6,7,8,9,1,2,3],
+                                 [7,8,9,1,2,3,4,5,6],
+                                 [2,3,1,5,6,4,8,9,7],
+                                 [5,6,4,8,9,7,2,3,1],
+                                 [8,9,7,2,3,1,5,6,4],
+                                 [3,1,2,6,4,5,9,7,8],
+                                 [6,4,5,9,7,8,3,1,2],
+                                 [9,7,8,3,1,2,6,4,5]])
+        self.sudoku = SuDoKu(solved_field)
+
+    def test_is_solved(self):
+        print(self.sudoku)
+        return
