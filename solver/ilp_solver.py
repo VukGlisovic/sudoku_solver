f092085fd11d25f389926de856f9ccc8e60a6d9e
¨Vuk <GlisovicVuk@gmail.com>
2017-12-16 22:44:04 +0100

    fixed adding positional constraints

--- solver/ilp_solver.py
+++ solver/ilp_solver.py
@@ -10,7 +10,7 @@ class ILPsolver:
         for i in range(9):  # Indicates row
             for j in range(9):  # Indicates column
                 for k in range(1, 10):  # Indicates number from 1 till 9
-                    var_name = 'x{row}{col}{nr}'.format(row=1, col=j, nr=k)
+                    var_name = 'x{row}{col}{nr}'.format(row=i, col=j, nr=k)
                     self.var_dict[var_name] = pulp.LpVariable(var_name, lowBound=0, upBound=1, cat=pulp.LpInteger)
 
     def create_constraints(self):
@@ -18,15 +18,9 @@ class ILPsolver:
         for i in range(9):
             for j in range(9):
                 constraint_var_names = ['x{row}{col}{nr}'.format(row=i, col=j, nr=nr) for nr in range(1, 10)]
-                lp_variables = [self.var_dict[name] for name in self.var_dict.keys() if name in constraint_var_names]
+                lp_variables = [self.var_dict[name] for name in constraint_var_names]
                 c = sum(lp_variables) == 1
-                # c = var_dict['x' + str(i) + str(j) + '1'] + var_dict['x' + str(i) + str(j) + '2'] + var_dict[
-                #     'x' + str(i) + str(j) + '3'] \
-                #     + var_dict['x' + str(i) + str(j) + '4'] + var_dict['x' + str(i) + str(j) + '5'] + var_dict[
-                #         'x' + str(i) + str(j) + '6'] \
-                #     + var_dict['x' + str(i) + str(j) + '7'] + var_dict['x' + str(i) + str(j) + '8'] + var_dict[
-                #         'x' + str(i) + str(j) + '9'] == 1
-                self.problem += c, "pos_constr_{row}{col}".format(row=i, col=j)
+                self.problem += c, "pos_constr_{row}{col}".format(row=i, col=j)  # give the constraint a name
 
     def create_objective_function(self):
         pass
--- solver/tests/test_sudoku.py
+++ solver/tests/test_sudoku.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 
 from solver.create import SuDoKu
+from solver.ilp_solver import ILPsolver
 
 
 class TestSuDoKu(unittest.TestCase):
@@ -16,11 +17,29 @@ class TestSuDoKu(unittest.TestCase):
                                  [3,1,2,6,4,5,9,7,8],
                                  [6,4,5,9,7,8,3,1,2],
                                  [9,7,8,3,1,2,6,4,5]])
-        self.sudoku = SuDoKu(solved_field)
+
+        unsolved_field = np.array([[1,2,3,4,5,6,7,8,9],
+                                   [4,5,6,7,8,9,1,2,3],
+                                   [7,8,9,1,2,3,4,5,6],
+                                   [2,3,1,5,6,4,8,9,7],
+                                   [5,6,4,8,9,7,2,3,1],
+                                   [8,9,7,2,3,1,5,6,4],
+                                   [3,1,2,6,4,5,9,7,8],
+                                   [6,4,5,9,7,8,3,1,2],
+                                   [9,7,8,3,1,2,6,4,np.nan]])
+        self.solved_sudoku = SuDoKu(solved_field)
+        self.unsolved_sudoku = SuDoKu(unsolved_field)
 
     def test_is_solved(self):
-        solved = self.sudoku.is_solved()
+        solved = self.solved_sudoku.is_solved()
         self.assertEqual(solved, True)
+        solved = self.unsolved_sudoku.is_solved()
+        self.assertEqual(solved, False)
+
+    def test_create_constraints(self):
+        solver = ILPsolver()
+        solver.create_variables()
+        solver.create_constraints()
 
 
 if __name__ == '__main__':
