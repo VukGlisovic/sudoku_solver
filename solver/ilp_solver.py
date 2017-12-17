eddde1065847328267c7d9e581e5fa19bbc15621
¨Vuk <GlisovicVuk@gmail.com>
2017-12-17 20:49:27 +0100

    Added other constraints

--- solver/ilp_solver.py
+++ solver/ilp_solver.py
@@ -14,13 +14,36 @@ class ILPsolver:
                     self.var_dict[var_name] = pulp.LpVariable(var_name, lowBound=0, upBound=1, cat=pulp.LpInteger)
 
     def add_constraints(self):
-        # positional constraints
+        # positional constraints (every position can have only one number)
         for i in range(9):
             for j in range(9):
                 constraint_var_names = ['x{row}{col}{nr}'.format(row=i, col=j, nr=nr) for nr in range(1, 10)]
                 lp_variables = [self.var_dict[name] for name in constraint_var_names]
                 c = sum(lp_variables) == 1
-                self.problem += c, "pos_constr_{row}{col}".format(row=i, col=j)  # give the constraint a name
+                self.problem += c, "positional_constraint_{row}{col}".format(row=i, col=j)  # give the constraint a name
+        # rowwise constraints (every number can occur only once in a row)
+        for i in range(9):
+            for k in range(1, 10):
+                constraint_var_names = ['x{row}{col}{nr}'.format(row=i, col=j, nr=k) for j in range(9)]
+                lp_variables = [self.var_dict[name] for name in constraint_var_names]
+                c = sum(lp_variables) == 1
+                self.problem += c, "row_constraint_{row}{nr}".format(row=i, nr=k)
+        # columnwise constraints (every number can occur only once in a column)
+        for j in range(9):
+            for k in range(1, 10):
+                constraint_var_names = ['x{row}{col}{nr}'.format(row=i, col=j, nr=k) for i in range(9)]
+                lp_variables = [self.var_dict[name] for name in constraint_var_names]
+                c = sum(lp_variables) == 1
+                self.problem += c, "column_constraint_{col}{nr}".format(col=j, nr=k)
+        # boxwise constraints (every number can occur only once in a box)
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                for k in range(1, 10):
+                    constraint_var_names = ['x{row}{col}{nr}'.format(row=i+p, col=j+q, nr=k) for p in range(3) for q in range(3)]
+                    lp_variables = [self.var_dict[name] for name in constraint_var_names]
+                    c = sum(lp_variables) == 1
+                    self.problem += c, "box_constraint_{row}{col}{nr}".format(row=i, col=j, nr=k)
+
 
     def create_objective_function(self):
         pass
