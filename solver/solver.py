489418476a4f98cbc49d82aa0764d32bcace5b18
¨Vuk <GlisovicVuk@gmail.com>
2018-03-25 11:22:10 +0200

    Removed module since it's replaced by an abstractclass

--- solver/solver.py
+++ solver/solver.py
@@ -6,6 +6,8 @@ import numpy as np
 import logging
 
 logger = logging.getLogger()
+log_format = '%(asctime)s | %(levelname)s | %(filename)s:%(lineno)s - %(funcName)s | %(message)s'
+logging.basicConfig(format=log_format, level=logging.INFO)
 
 
 logger.info("Hello numpy {}".format(np.float(6)))
