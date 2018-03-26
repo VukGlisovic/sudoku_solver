8d355b3741679eb39a918fbc9c02492179a3e9cd
¨Vuk <GlisovicVuk@gmail.com>
2018-03-26 21:38:33 +0200

    Started working on main module; created loader for class that will be used as a solver

--- solver/solver.py
+++ solver/solver.py
@@ -1,8 +1,7 @@
 """
 Run this script with the the wanted input and get your output
 """
-
-import numpy as np
+import importlib
 import logging
 
 logger = logging.getLogger()
@@ -10,4 +9,24 @@ log_format = '%(asctime)s | %(levelname)s | %(filename)s:%(lineno)s - %(funcName
 logging.basicConfig(format=log_format, level=logging.INFO)
 
 
-logger.info("Hello numpy {}".format(np.float(6)))
+def load_strategy_class(name):
+    split_result = name.split('.')
+    if len(split_result) != 2:
+        raise ValueError("Expected following format for input: <module_name>.<class_name>")
+    module_name, class_name = split_result
+    logger.info("Loading module strategies.{}.".format(module_name))
+    module = importlib.import_module("strategies.{}".format(module_name))
+    logger.info("Loading solver class from retrieved module.")
+    solver_class = getattr(module, class_name, None)
+    if not solver_class:
+        raise ValueError("Could not find class {} in module strategies.{}.".format(class_name, module_name))
+    return solver_class
+
+
+def main():
+    module = load_strategy_class('ilp_solver.ILPsolvers')
+    return
+
+
+if __name__ == '__main__':
+    main()
